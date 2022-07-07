c6069f90a7ad0f46a69bb92a9713a925f0d94619
mineunyoung <bear0369@naver.com>
2022-07-07 11:35:33 +0900

    feat: update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Button from '@/components/common/button';
+import Divider from '@/components/common/divider';
+import Input from '@/components/common/input/input';
+import InputToggle from '@/components/common/input/inputToggle';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -15,189 +18,84 @@ const Index = () => {
       }
     >
       <h1 className="text-2xl font-bold">Cakestation Design System</h1>
-      <p>
-        <span role="img" aria-label="rocket">
-          🚀
-        </span>{' '}
-        BUTTON{' '}
-        <Button size={'full'} category={'primary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'full'} category={'secondary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'full'} category={'primary'} disabled>
-          텍스트
-        </Button>
-        <Button size={'medium'} category={'primary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'medium'} category={'secondary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'medium'} category={'primary'} disabled>
-          텍스트
-        </Button>
-        <Button size={'small'} category={'primary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'small'} category={'secondary'} disabled={false}>
-          텍스트
-        </Button>
-        <Button size={'small'} category={'primary'} disabled>
-          텍스트
-        </Button>
-      </p>
-      <p>
-        <span role="img" aria-label="zap">
-          ⚡️
-        </span>{' '}
-        INPUT{' '}
-      </p>
-
-      <h2 className="text-lg font-semibold">Next js Boilerplate Features</h2>
-      <p>Developer experience first:</p>
-      <ul>
-        <li>
-          <span role="img" aria-label="fire">
-            🔥
-          </span>{' '}
-          <a href="https://nextjs.org" rel="nofollow">
-            Next.js
-          </a>{' '}
-          for Static Site Generator
-        </li>
-        <li>
-          <span role="img" aria-label="art">
-            🎨
-          </span>{' '}
-          Integrate with{' '}
-          <a href="https://tailwindcss.com" rel="nofollow">
-            Tailwind CSS
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="nail_care">
-            💅
-          </span>{' '}
-          PostCSS for processing Tailwind CSS
-        </li>
-        <li>
-          <span role="img" aria-label="tada">
-            🎉
-          </span>{' '}
-          Type checking Typescript
-        </li>
-        <li>
-          <span role="img" aria-label="pencil2">
-            ✏️
-          </span>{' '}
-          Linter with{' '}
-          <a href="https://eslint.org" rel="nofollow">
-            ESLint
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="hammer_and_wrench">
-            🛠
-          </span>{' '}
-          Code Formatter with{' '}
-          <a href="https://prettier.io" rel="nofollow">
-            Prettier
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="fox_face">
-            🦊
-          </span>{' '}
-          Husky for Git Hooks
-        </li>
-        <li>
-          <span role="img" aria-label="no_entry_sign">
-            🚫
-          </span>{' '}
-          Lint-staged for running linters on Git staged files
-        </li>
-        <li>
-          <span role="img" aria-label="no_entry_sign">
-            🗂
-          </span>{' '}
-          VSCode configuration: Debug, Settings, Tasks and extension for
-          PostCSS, ESLint, Prettier, TypeScript
-        </li>
-        <li>
-          <span role="img" aria-label="robot">
-            🤖
-          </span>{' '}
-          SEO metadata, JSON-LD and Open Graph tags with Next SEO
-        </li>
-        <li>
-          <span role="img" aria-label="robot">
-            ⚙️
-          </span>{' '}
-          <a
-            href="https://www.npmjs.com/package/@next/bundle-analyzer"
-            rel="nofollow"
-          >
-            Bundler Analyzer
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="rainbow">
-            🌈
-          </span>{' '}
-          Include a FREE minimalist theme
-        </li>
-        <li>
-          <span role="img" aria-label="hundred">
-            💯
-          </span>{' '}
-          Maximize lighthouse score
-        </li>
-      </ul>
-      <p>Built-in feature from Next.js:</p>
-      <ul>
-        <li>
-          <span role="img" aria-label="coffee">
-            ☕
-          </span>{' '}
-          Minify HTML &amp; CSS
-        </li>
-        <li>
-          <span role="img" aria-label="dash">
-            💨
-          </span>{' '}
-          Live reload
-        </li>
-        <li>
-          <span role="img" aria-label="white_check_mark">
-            ✅
-          </span>{' '}
-          Cache busting
-        </li>
-      </ul>
-      <h2 className="text-lg font-semibold">Our Stater code Philosophy</h2>
-      <ul>
-        <li>Minimal code</li>
-        <li>SEO-friendly</li>
-        <li>
+      <div>
+        <p>
           <span role="img" aria-label="rocket">
             🚀
           </span>{' '}
-          Production-ready
-        </li>
-      </ul>
-      <p>
-        Check our GitHub project for more information about{' '}
-        <a href="https://github.com/ixartz/Next-js-Boilerplate">
-          Nextjs Boilerplate
-        </a>
-        . You can also browse our{' '}
-        <a href="https://creativedesignsguru.com/category/nextjs/">
-          Premium NextJS Templates
-        </a>{' '}
-        on our website to support this project.
-      </p>
+          BUTTON
+        </p>
+        <div>
+          <Button size={'full'} category={'primary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'full'} category={'secondary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'full'} category={'primary'} disabled>
+            텍스트
+          </Button>
+        </div>
+        <div>
+          <Button size={'medium'} category={'primary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'medium'} category={'secondary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'medium'} category={'primary'} disabled>
+            텍스트
+          </Button>
+        </div>
+        <div>
+          <Button size={'small'} category={'primary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'small'} category={'secondary'} disabled={false}>
+            텍스트
+          </Button>
+          <Button size={'small'} category={'primary'} disabled>
+            텍스트
+          </Button>
+        </div>
+      </div>
+      <div>
+        <p>
+          <span role="img" aria-label="zap">
+            ⚡️
+          </span>{' '}
+          INPUT
+        </p>
+        <div>
+          <Input placeholder={'내용을 입력하세요.'} onChange={undefined} />
+        </div>
+      </div>
+      <div>
+        <p>
+          <span role="img" aria-label="zap">
+            🔥
+          </span>{' '}
+          INPUT TOGGLE
+        </p>
+        <div>
+          <InputToggle
+            options={['첫번째 옵션', '두번째 옵션', '세번째 옵션']}
+          />
+        </div>
+      </div>
+      <div>
+        <p>
+          <span role="img" aria-label="zap">
+            👑
+          </span>{' '}
+          Divider
+        </p>
+        <div>
+          <Divider size={'lg'} />
+          <Divider size={'md'} />
+          <Divider size={'sm'} />
+        </div>
+      </div>
     </Main>
   );
 };

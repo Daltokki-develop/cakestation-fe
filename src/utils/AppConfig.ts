b1d95925b9b5063cd60490110a6adda18a479644
mineunyoung <bear0369@naver.com>
2022-07-07 11:33:23 +0900

    feat: update appconfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Cake Station FE',
+  site_name: 'Cakestation',
+  title: 'Cake Station',
   description: '케이크 스테이션 프론트엔드 진행 상황',
   locale: 'kr',
 };

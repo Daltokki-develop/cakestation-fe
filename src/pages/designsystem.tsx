f4302c88f94754291f13269d309db91184d5bbb8
Yuri Lee <yurilee.tech@gmail.com>
2022-07-10 23:15:59 +0900

    feat: update DesignSystem component WIP

--- src/pages/designsystem.tsx
+++ src/pages/designsystem.tsx
@@ -5,7 +5,9 @@ import Divider from '@/components/common/divider';
 import Input from '@/components/common/input/input';
 import InputToggle from '@/components/common/input/inputToggle';
 import Tag from '@/components/common/tag';
+import Typography from '@/components/common/typography';
 import { Meta } from '@/layouts/Meta';
+import palette from '@/styles/palette';
 import { Main } from '@/templates/Main';
 
 const DesignSystem = () => {
@@ -143,6 +145,15 @@ const DesignSystem = () => {
           </Tag>
         </div>
       </div>
+      <Typography category={'H1'}>H1</Typography>
+      <Typography category={'H2'}>H2</Typography>
+      <Typography category={'H3'}>H3</Typography>
+      <Typography category={'H4'}>H4</Typography>
+      <Typography category={'H5'}>H5</Typography>
+      <Typography category={'H6'}>H6</Typography>
+      <Typography category={'H7'} color={palette.cakeLavender_500}>
+        H7
+      </Typography>
     </Main>
   );
 };

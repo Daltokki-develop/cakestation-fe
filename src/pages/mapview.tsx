21b1133345dbb2132e2736711d39d382edebbd98
mineunyoung <bear0369@naver.com>
2022-08-01 21:53:42 +0900

    chore: update mapview searchbar props

--- src/pages/mapview.tsx
+++ src/pages/mapview.tsx
@@ -42,7 +42,10 @@ const Mapview = () => {
   return (
     <Main meta={<Meta title="Cakestation Map" description="지도 맛보기" />}>
       <Header style={'bar'}>
-        <SearchBar placeholder="가게와 가까운 지하철 역 검색" />
+        <SearchBar
+          placeholder="가게와 가까운 지하철 역 검색"
+          onChange={() => {}}
+        />
       </Header>
       <Absolute>
         <Map

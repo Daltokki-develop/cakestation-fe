4fe6a0d434e9d4a7b19a8c57bcc3da666d0bde4c
mineunyoung <bear0369@naver.com>
2022-08-03 08:35:24 +0900

    refactor: reviews write order page refactoring

--- src/pages/reviews/write/[id]/order.tsx
+++ src/pages/reviews/write/[id]/order.tsx
@@ -6,15 +6,12 @@ import InputToggle from '@/components/common/input/inputToggle';
 import Typography from '@/components/common/typography';
 import { Meta } from '@/layouts/Meta';
 import { Review } from '@/layouts/Review';
+import cakeNum from '@/lib/케이크호수.json';
 import { Main } from '@/templates/Main';
 
 const Order = () => {
   const router = useRouter();
   const { id } = router.query;
-
-  const GoNext = () => {
-    console.log(`가게 ID : ${id}\n`);
-  };
 
   return (
     <Main meta={<Meta title="Cakestation Review" description="리뷰 맛보기" />}>
@@ -23,7 +20,9 @@ const Order = () => {
         title={'주문 내용'}
         subtitle={'주문하셨던 케이크의 정보를 입력해주세요.'}
         nextText={'다음'}
-        nextFunc={GoNext}
+        nextFunc={() => {
+          console.log(`가게 ID : ${id}\n`);
+        }}
         nextLink={`/reviews/write/${id}/satisfaction/`}
       >
         <div className="w-85">
@@ -31,32 +30,16 @@ const Order = () => {
             <div className="mb-8">
               <Typography category={'Bd2'}>케이크의 호수</Typography>
             </div>
-            <InputToggle
-              options={[
-                '1호',
-                '2호',
-                '3호',
-                '4호',
-                '5호',
-                '6호',
-                '7호',
-                '8호',
-                '9호',
-              ]}
-            />
+            <InputToggle options={cakeNum} />
           </div>
-          <div className="mb-20">
-            <div className="mb-8">
-              <Typography category={'Bd2'}>케이크 시트의 종류</Typography>
+          {['케이크 시트의 종류', '추가 옵션'].map((title, index) => (
+            <div key={index} className="mb-20">
+              <div className="mb-8">
+                <Typography category={'Bd2'}>{title}</Typography>
+              </div>
+              <Input placeholder={''} onChange={() => {}} />
             </div>
-            <Input placeholder={''} onChange={() => {}} />
-          </div>
-          <div className="mb-20">
-            <div className="mb-8">
-              <Typography category={'Bd2'}>추가 옵션</Typography>
-            </div>
-            <Input placeholder={''} onChange={() => {}} />
-          </div>
+          ))}
         </div>
       </Review>
     </Main>

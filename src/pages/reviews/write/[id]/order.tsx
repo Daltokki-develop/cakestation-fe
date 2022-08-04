0cfaacba6104ffc24f367778e26510d33bf3e1e7
mineunyoung <bear0369@naver.com>
2022-08-04 14:05:06 +0900

    chore: add required mark

--- src/pages/reviews/write/[id]/order.tsx
+++ src/pages/reviews/write/[id]/order.tsx
@@ -26,20 +26,24 @@ const Order = () => {
         nextLink={`/reviews/write/${id}/satisfaction/`}
       >
         <div className="w-85">
-          <div className="mb-26">
+          <div className="mt-75 mb-26 required">
             <div className="mb-8">
               <Typography category={'Bd2'}>케이크의 호수</Typography>
             </div>
             <InputToggle options={cakeNum} />
           </div>
-          {['케이크 시트의 종류', '추가 옵션'].map((title, index) => (
-            <div key={index} className="mb-20">
-              <div className="mb-8">
-                <Typography category={'Bd2'}>{title}</Typography>
-              </div>
-              <Input placeholder={''} onChange={() => {}} />
+          <div className="mb-20 required">
+            <div className="mb-8">
+              <Typography category={'Bd2'}>케이크 시트의 종류</Typography>
             </div>
-          ))}
+            <Input placeholder={''} onChange={() => {}} />
+          </div>
+          <div className="mb-20">
+            <div className="mb-8">
+              <Typography category={'Bd2'}>추가 옵션</Typography>
+            </div>
+            <Input placeholder={''} onChange={() => {}} />
+          </div>
         </div>
       </Review>
     </Main>

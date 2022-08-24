912c89c7943f5dab879d165931616b337c17724d
mineunyoung <bear0369@naver.com>
2022-08-24 11:32:04 +0900

    feat: update checkbox component

--- src/components/common/checkbox.tsx
+++ src/components/common/checkbox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import palette from '@/styles/palette';
@@ -9,23 +9,16 @@ interface ICheckBoxProps {
   value?: string;
   checked?: boolean;
   onChange?: any;
+  checkedItems?: any;
 }
 
-const StyledLabel = styled.div`
-  display: flex;
-
-  & + & {
-    margin-left: 6px;
-  }
-`;
-
 const HiddenCheck = styled.input<ICheckBoxProps>`
-  /* visibility: hidden;
+  visibility: hidden;
   width: 0;
-  height: 0; */
+  height: 0;
 `;
 
-const StyledCheckBox = styled.div<ICheckBoxProps>`
+const StyledCheckBox = styled.label<ICheckBoxProps>`
   background: ${(props) =>
     props.checked ? `${palette.cakeLemon_400}` : `${palette.grey_100}`};
   border: 1px solid ${palette.black};
@@ -42,27 +35,36 @@ const StyledCheckBox = styled.div<ICheckBoxProps>`
   height: fit-content;
 
   cursor: pointer;
+
+  & + & {
+    margin-left: 6px;
+  }
 `;
 
 const CheckBox = (props: ICheckBoxProps) => {
   const [checked, setChecked] = useState(props.checked);
+  const handleCheck = ({ target }: { target: any }) => {
+    props.onChange(target.checked, target.value);
+    setChecked(target.checked);
+  };
+
+  useEffect(() => {
+    if (props.checkedItems.includes(props.value)) {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [props.checkedItems]);
   return (
-    <StyledLabel>
+    <StyledCheckBox checked={checked}>
       <HiddenCheck
         type="checkbox"
         value={props.value}
         checked={checked}
-        onChange={props.onChange}
+        onChange={({ e }: { e: any }) => handleCheck(e)}
       />
-      <StyledCheckBox
-        checked={checked}
-        onClick={() => {
-          setChecked(!checked);
-        }}
-      >
-        <Typography category={'Bd7'}>{props.value}</Typography>
-      </StyledCheckBox>
-    </StyledLabel>
+      <Typography category={'Bd7'}>{props.value}</Typography>
+    </StyledCheckBox>
   );
 };
 

642c973470406cfc92db0fa50939559bff6df2ba
mineunyoung <bear0369@naver.com>
2022-08-24 11:42:35 +0900

    feat: update rate system and checkbox

--- src/pages/reviews/write/[id]/general.tsx
+++ src/pages/reviews/write/[id]/general.tsx
@@ -2,15 +2,15 @@ import 'rc-rate/assets/index.css';
 
 import { useRouter } from 'next/router';
 import Rate from 'rc-rate';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import CheckBox from '@/components/common/checkbox';
 import TextArea from '@/components/common/input/textArea';
 import Typography from '@/components/common/typography';
 import { Meta } from '@/layouts/Meta';
 import { Review } from '@/layouts/Review';
 import tags from '@/lib/총평태그.json';
-import palette from '@/styles/palette';
 import { Main } from '@/templates/Main';
 
 const StyledRate = styled(Rate)`
@@ -20,39 +20,25 @@ const StyledRate = styled(Rate)`
   justify-content: center;
   font-size: 40px;
 
-  .rc-rate-star-half .rc-rate-star-first,
+  /* .rc-rate-star-half .rc-rate-star-first,
   .rc-rate-star-full .rc-rate-star-second {
     color: #363636 !important;
-  }
-`;
-
-interface IReviewTagProps {
-  clicked?: boolean;
-}
-
-const ReviewTag = styled.div<IReviewTagProps>`
-  background: ${(props) =>
-    props.clicked ? `${palette.cakeLemon_400}` : `${palette.grey_100}`};
-  border: 1px solid ${palette.black};
-  padding: 10px 12px;
-  margin: 3px;
-  white-space: nowrap;
-  border-radius: 16px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: fit-content;
-
-  & + & {
-    margin-left: 6px;
-  }
+  } */
 `;
 
 const General = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  const [checkedList, setCheckedList] = useState<Array<string>>([]);
+
+  const HandleCheckList = (checked: boolean, item: any) => {
+    if (checked) {
+      setCheckedList([...checkedList, item]);
+    } else if (!checked) {
+      setCheckedList(checkedList.filter((el) => el !== item));
+    }
+  };
 
   return (
     <Main meta={<Meta title="Cakestation Review" description="리뷰 맛보기" />}>
@@ -67,7 +53,11 @@ const General = () => {
         nextLink={`/`}
       >
         <div className="w-85">
-          <StyledRate allowHalf />
+          <StyledRate
+            character={
+              <img src="/assets/images/icons/rate_filled.svg" alt="rate" />
+            }
+          />
           <div className="w-100 mb-21 text-center">
             <Typography category={'Bd2'}>
               좋았던 점을 체크해주세요 (중복가능)
@@ -75,9 +65,13 @@ const General = () => {
           </div>
           <div className="row contents-center mb-80 flex-wrap">
             {tags.map((tag, index) => (
-              <ReviewTag key={index}>
-                <Typography category={'Bd7'}>{tag}</Typography>
-              </ReviewTag>
+              <CheckBox
+                key={index}
+                value={tag}
+                onChange={HandleCheckList}
+                checked={!!checkedList.includes(tag)}
+                checkedItems={checkedList}
+              />
             ))}
           </div>
           <div className="w-100 mb-24 text-center">

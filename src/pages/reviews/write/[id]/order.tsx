56f2ff82f7e6c107cd8e4dd9ed5d13e31b0bf05b
mineunyoung <bear0369@naver.com>
2022-08-30 11:06:25 +0900

    refactor: refactoring order page

--- src/pages/reviews/write/[id]/order.tsx
+++ src/pages/reviews/write/[id]/order.tsx
@@ -11,14 +11,57 @@ import cakeMenu from '@/lib/케이크시트종류.json';
 import cakeSize from '@/lib/케이크호수.json';
 import { Main } from '@/templates/Main';
 
+const CakeSize = (size: string, HandleSize: (e: any) => void) => {
+  return (
+    <div className="mt-75 mb-26 required">
+      <div className="mb-8">
+        <Typography category={'Bd2'}>케이크의 호수</Typography>
+      </div>
+      <InputToggle value={size} options={cakeSize} onChange={HandleSize} />
+    </div>
+  );
+};
+
+const CakeSheet = (
+  menu: string,
+  HandleMenu: (e: any) => void,
+  HandleEtc: (e: any) => void
+) => {
+  return (
+    <div className="mb-20 required">
+      <div className="mb-8">
+        <Typography category={'Bd2'}>케이크 시트의 종류</Typography>
+      </div>
+      <InputToggle value={menu} options={cakeMenu} onChange={HandleMenu} />
+      {menu === '직접 입력' && (
+        <div className="mt-8">
+          <Input
+            placeholder={'케이크 시트의 종류를 직접 입력해주세요.'}
+            onChange={HandleEtc}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const AdditionalOption = () => {
+  return (
+    <div className="mb-20">
+      <div className="mb-8">
+        <Typography category={'Bd2'}>추가 옵션</Typography>
+      </div>
+      <TextArea placeholder={''} onChange={() => {}} />
+    </div>
+  );
+};
+
 const Order = () => {
   const router = useRouter();
   const { id } = router.query;
 
   const [size, setSize] = useState<string>(cakeSize[0] || '');
-
   const [menu, setMenu] = useState<string>(cakeMenu[0] || '');
-
   const [etc, setEtc] = useState<string>('');
 
   const HandleSize = (e: any) => {
@@ -37,7 +80,7 @@ const Order = () => {
   return (
     <Main meta={<Meta title="Cakestation Review" description="리뷰 맛보기" />}>
       <Review
-        progress={'60%'}
+        progress={'50%'}
         title={'주문 내용'}
         subtitle={'주문하셨던 케이크의 정보를 입력해주세요.'}
         nextText={'다음'}
@@ -47,40 +90,9 @@ const Order = () => {
         nextLink={`/reviews/write/${id}/satisfaction/`}
       >
         <div className="w-85">
-          <div className="mt-75 mb-26 required">
-            <div className="mb-8">
-              <Typography category={'Bd2'}>케이크의 호수</Typography>
-            </div>
-            <InputToggle
-              value={size}
-              options={cakeSize}
-              onChange={HandleSize}
-            />
-          </div>
-          <div className="mb-20 required">
-            <div className="mb-8">
-              <Typography category={'Bd2'}>케이크 시트의 종류</Typography>
-            </div>
-            <InputToggle
-              value={menu}
-              options={cakeMenu}
-              onChange={HandleMenu}
-            />
-            {menu === '직접 입력' && (
-              <div className="mt-8">
-                <Input
-                  placeholder={'케이크 시트의 종류를 직접 입력해주세요.'}
-                  onChange={HandleEtc}
-                />
-              </div>
-            )}
-          </div>
-          <div className="mb-20">
-            <div className="mb-8">
-              <Typography category={'Bd2'}>추가 옵션</Typography>
-            </div>
-            <TextArea placeholder={''} onChange={() => {}} />
-          </div>
+          {CakeSize(size, HandleSize)}
+          {CakeSheet(menu, HandleMenu, HandleEtc)}
+          {AdditionalOption()}
         </div>
       </Review>
     </Main>

c13777a860cd6b809e76e8012301b8d2b36e523c
mineunyoung <bear0369@naver.com>
2022-08-30 11:37:09 +0900

    feat: update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,45 +3,53 @@ import create from 'zustand';
 const useStore = create((set) => ({
   reviews: [
     {
-      nearByStation: '회기',
-      walkingDistance: '5분',
       photo: [],
       cakeNumber: '1호',
       sheetType: '레몬',
       requestOption: '추가 옵션',
+      satisfaction: '매우 만족해요!',
       score: 4.5,
       tags: ['직원이 친절해요', '역과 가까워요'],
       content: '하고싶은 말',
     },
   ],
-  addReview: (review: any) =>
-    set((state: any) => ({
-      reviews: [
-        {
-          nearByStation: review.nearByStation,
-          walkingDistance: review.walkingDistance,
-          photo: review.photo,
-          cakeNumber: review.cakeNumber,
-          sheetType: review.sheetType,
-          requestOption: review.requestOption,
-          score: review.score,
-          tags: review.tags,
-          content: review.content,
-        },
-        ...state.reviews,
-      ],
-    })),
-  removeReview: (id: any) =>
-    set((state: any) => ({
-      reviews: state.reviews.filter((review: any) => review.id !== id),
-    })),
-  updateDistance: (review: any) =>
+  updateAddPictures: (review: any) =>
     set((state: any) => ({
       reviews: state.reviews.map((item: any) => {
         return {
           ...item,
-          nearByStation: review.nearByStation,
-          walkingDistance: review.walkingDistance,
+          photo: review.photo,
+        };
+      }),
+    })),
+  updateOrder: (review: any) =>
+    set((state: any) => ({
+      reviews: state.reviews.map((item: any) => {
+        return {
+          ...item,
+          cakeNumber: review.cakeNumber,
+          sheetType: review.sheetType,
+          requestOption: review.requestOption,
+        };
+      }),
+    })),
+  updateSatisfaction: (review: any) =>
+    set((state: any) => ({
+      reviews: state.reviews.map((item: any) => {
+        return {
+          ...item,
+          satisfaction: review.satisfaction,
+        };
+      }),
+    })),
+  updateGeneral: (review: any) =>
+    set((state: any) => ({
+      reviews: state.reviews.map((item: any) => {
+        return {
+          ...item,
+          score: review.score,
+          tags: review.tags,
+          content: review.content,
         };
       }),
     })),

7199d7fb80ff0e7961feadc95c3ba2926aa026de
mineunyoung <bear0369@naver.com>
2022-08-30 09:58:04 +0900

    feat: update itemcard for simple

--- src/components/common/itemcard.tsx
+++ src/components/common/itemcard.tsx
@@ -9,26 +9,32 @@ interface IItemCardProps {
   line?: boolean;
   title: string;
   icon?: string;
-  rate: string;
-  count: number;
+  rate?: string;
+  count?: number;
   distance?: string;
-  pictures: string[];
+  pictures?: string[];
   heart?: boolean;
+  // simplecard
+  isSimple?: boolean;
+  location?: string;
+  onClick?: () => void;
 }
 
 interface IStyledItemCardProps {
   line?: boolean;
+  isSimple?: boolean;
 }
 
 const StyledItemCard = styled.div<IStyledItemCardProps>`
-  width: 100%;
+  width: ${(props) => (props.isSimple ? 'calc(100% - 2rem)' : '100%')};
   border: ${(props) =>
     props.line ? `.0625rem solid ${palette.black}` : 'none'};
   border-radius: ${(props) => (props.line ? '1rem' : 0)};
-  padding: 1.5rem 0 0;
+  padding: ${(props) => (props.isSimple ? '1rem' : '1.5rem 0 0')};
 
   display: flex;
   flex-direction: column;
+  align-items: ${(props) => (props.isSimple ? 'flex-start' : 'normal')};
 
   background-color: ${palette.white};
   overflow: hidden;
@@ -37,6 +43,10 @@ const StyledItemCard = styled.div<IStyledItemCardProps>`
 
   &:hover {
     background-color: ${palette.grey_100};
+  }
+
+  & + & {
+    margin-top: 10px;
   }
 `;
 
@@ -94,64 +104,85 @@ const EmptyPicture = styled.div`
   background-color: ${palette.grey_200};
 `;
 
-const ItemCard = (props: IItemCardProps) => {
-  const AddEmptyPicture = () => {
-    const pictureArray = [];
-    for (let index = 0; index < 4 - props.pictures.length; index += 1)
-      pictureArray.push(<EmptyPicture key={index} />);
-    return pictureArray;
-  };
+const AddEmptyPicture = (props: IItemCardProps) => {
+  const pictureArray = [];
+  const picSize = props.pictures?.length || 0;
+  for (let index = 0; index < 4 - picSize; index += 1)
+    pictureArray.push(<EmptyPicture key={index} />);
+  return pictureArray;
+};
 
+const ItemCard = (props: IItemCardProps) => {
   return (
-    <StyledItemCard line={props.line}>
-      <ItemCardHeader>
-        <ItemCardTitle>
+    <StyledItemCard
+      isSimple={props.isSimple}
+      line={props.line}
+      onClick={props.onClick}
+    >
+      {props.isSimple ? (
+        <>
           <Typography category={'Bd1'} color={'grey_800'}>
             {props.title}
           </Typography>
-          <ItemCardDesc>
-            <RateImage src="/assets/images/icons/rate_filled.svg" alt="rate" />
+          <Typography category={'Bd7'} color={'grey_800'}>
+            {props.location}
+          </Typography>
+        </>
+      ) : (
+        <>
+          <ItemCardHeader>
+            <ItemCardTitle>
+              <Typography category={'Bd1'} color={'grey_800'}>
+                {props.title}
+              </Typography>
+              <ItemCardDesc>
+                <RateImage
+                  src="/assets/images/icons/rate_filled.svg"
+                  alt="rate"
+                />
 
-            <RateText>
-              <Typography category={'Bd7'} color={'grey_800'}>
-                {props.rate}
-              </Typography>
-            </RateText>
-            <CountText>
-              <Typography category={'Bd7'} color={'grey_400'}>
-                ({props.count})
-              </Typography>
-            </CountText>
-            {props.distance && (
-              <span>
-                <Typography category={'Bd6'} color={'blue_500'}>
-                  {props.distance}
-                </Typography>
-              </span>
+                <RateText>
+                  <Typography category={'Bd7'} color={'grey_800'}>
+                    {props.rate}
+                  </Typography>
+                </RateText>
+                <CountText>
+                  <Typography category={'Bd7'} color={'grey_400'}>
+                    ({props.count})
+                  </Typography>
+                </CountText>
+                {props.distance && (
+                  <span>
+                    <Typography category={'Bd6'} color={'blue_500'}>
+                      {props.distance}
+                    </Typography>
+                  </span>
+                )}
+              </ItemCardDesc>
+            </ItemCardTitle>
+            {props.heart && (
+              <div>
+                <HeartImage
+                  src="/assets/images/icons/heart_color-filled.svg"
+                  alt="heart"
+                />
+              </div>
             )}
-          </ItemCardDesc>
-        </ItemCardTitle>
-        {props.heart && (
-          <div>
-            <HeartImage
-              src="/assets/images/icons/heart_color-filled.svg"
-              alt="heart"
-            />
-          </div>
-        )}
-      </ItemCardHeader>
-      <Pictures>
-        {props.pictures.map((picture, index) => {
-          return (
-            <Picture
-              key={index}
-              src={`/assets/images/${picture}`}
-              alt="picture"
-            />
-          );
-        })}
-        {AddEmptyPicture()}
-      </Pictures>
+          </ItemCardHeader>
+          <Pictures>
+            {props.pictures?.map((picture, index) => {
+              return (
+                <Picture
+                  key={index}
+                  src={`/assets/images/${picture}`}
+                  alt="picture"
+                />
+              );
+            })}
+            {AddEmptyPicture(props)}
+          </Pictures>
+        </>
+      )}
     </StyledItemCard>
   );
 };

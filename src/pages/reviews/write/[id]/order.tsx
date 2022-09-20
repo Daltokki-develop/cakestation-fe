73c96d07a0efa6cda316a5496d20d1cf39a7971c
danbom <bear0369@naver.com>
2022-09-20 22:31:03 +0900

    feat: handle session storage in order page

--- src/pages/reviews/write/[id]/order.tsx
+++ src/pages/reviews/write/[id]/order.tsx
@@ -6,24 +6,28 @@ import InputToggle from '@/components/common/input/inputToggle';
 import Typography from '@/components/common/typography';
 import { Meta } from '@/layouts/Meta';
 import { Review } from '@/layouts/Review';
-import cakeMenu from '@/lib/케이크시트종류.json';
-import cakeSize from '@/lib/케이크호수.json';
+import cakeSheet from '@/lib/케이크시트종류.json';
+import cakeSizes from '@/lib/케이크호수.json';
 import { Main } from '@/templates/Main';
 
 const CakeSize = (size: string, HandleSize: (e: any) => void) => {
+  const option = useState<any>();
+  cakeSizes.forEach((cakeSize, i) => {
+    option[i] = `${cakeSize}호`;
+  });
   return (
     <div className="mt-75 mb-26 required">
       <div className="mb-8">
         <Typography category={'Bd2'}>케이크의 호수</Typography>
       </div>
-      <InputToggle value={size} options={cakeSize} onChange={HandleSize} />
+      <InputToggle value={size} options={option} onChange={HandleSize} />
     </div>
   );
 };
 
 const CakeSheet = (
-  menu: string,
-  HandleMenu: (e: any) => void,
+  sheet: string,
+  HandleSheet: (e: any) => void,
   HandleEtc: (e: any) => void
 ) => {
   return (
@@ -31,8 +35,8 @@ const CakeSheet = (
       <div className="mb-8">
         <Typography category={'Bd2'}>케이크 시트의 종류</Typography>
       </div>
-      <InputToggle value={menu} options={cakeMenu} onChange={HandleMenu} />
-      {menu === '직접 입력' && (
+      <InputToggle value={sheet} options={cakeSheet} onChange={HandleSheet} />
+      {sheet === '직접 입력' && (
         <div className="mt-8">
           <Input
             placeholder={'케이크 시트의 종류를 직접 입력해주세요.'}
@@ -44,36 +48,37 @@ const CakeSheet = (
   );
 };
 
-const AdditionalOption = () => {
-  return (
-    <div className="mb-20">
-      <div className="mb-8">
-        <Typography category={'Bd2'}>추가 옵션</Typography>
-      </div>
-      <Input textarea placeholder={''} onChange={() => {}} />
-    </div>
-  );
-};
-
 const Order = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  const [size, setSize] = useState<string>(cakeSize[0] || '');
-  const [menu, setMenu] = useState<string>(cakeMenu[0] || '');
+  const [size, setSize] = useState<string>(`${cakeSizes[0]}호` || '');
+  const [sheet, setSheet] = useState<string>(cakeSheet[0] || '');
   const [etc, setEtc] = useState<string>('');
+  const [option, setOption] = useState<String>('');
 
   const HandleSize = (e: any) => {
     setSize(e.target.value);
   };
 
-  const HandleMenu = (e: any) => {
-    setMenu(e.target.value);
+  const HandleSheet = (e: any) => {
+    setSheet(e.target.value);
   };
 
   const HandleEtc = (e: any) => {
     setEtc(e.target.value);
-    console.log(etc);
+  };
+
+  const HandleOption = (e: any) => {
+    setOption(e.target.value);
+  };
+
+  const HandleNext = () => {
+    const sheetType = sheet === '직접 입력' ? etc : sheet;
+    sessionStorage.setItem(
+      'ReviewData',
+      JSON.stringify({ cakeNumber: size, sheetType, requestOption: option })
+    );
   };
 
   return (
@@ -83,15 +88,18 @@ const Order = () => {
         title={'주문 내용'}
         subtitle={'주문하셨던 케이크의 정보를 입력해주세요.'}
         nextText={'다음'}
-        nextFunc={() => {
-          console.log(`가게 ID : ${id}\n`);
-        }}
+        nextFunc={HandleNext}
         nextLink={`/reviews/write/${id}/satisfaction/`}
       >
         <div className="w-85">
           {CakeSize(size, HandleSize)}
-          {CakeSheet(menu, HandleMenu, HandleEtc)}
-          {AdditionalOption()}
+          {CakeSheet(sheet, HandleSheet, HandleEtc)}
+          <div className="mb-20">
+            <div className="mb-8">
+              <Typography category={'Bd2'}>추가 옵션</Typography>
+            </div>
+            <Input textarea placeholder={''} onChange={HandleOption} />
+          </div>
         </div>
       </Review>
     </Main>

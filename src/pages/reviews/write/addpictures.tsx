389826f781a8aa834bda5938164d74672fbe3c70
mineunyoung <bear0369@naver.com>
2022-09-29 10:29:25 +0900

    feat: upload button styling and add text for number of files

--- src/pages/reviews/write/addpictures.tsx
+++ src/pages/reviews/write/addpictures.tsx
@@ -1,11 +1,12 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import Typography from '@/components/common/typography';
 import { Meta } from '@/layouts/Meta';
 import { Review } from '@/layouts/Review';
 import { Main } from '@/templates/Main';
@@ -17,8 +18,18 @@ const StyledImage = styled.img`
   object-fit: cover;
 `;
 
+const StyledLabel = styled.label`
+  width: 292px;
+  height: 292px;
+  background-image: url('/assets/images/upload.png');
+  background-size: cover;
+  cursor: pointer;
+`;
+
 const AddPictures = () => {
   const [imageList, setImageList] = useState<any>([]);
+  const [fileList, setFileList] = useState<any>([]);
+  const imageInput = useRef(null);
 
   const onChangeImages = useCallback((e: any) => {
     const currentImageList: string | string[] = [];
@@ -29,11 +40,24 @@ const AddPictures = () => {
     });
     // @ts-ignore
     setImageList([...imageList, currentImageList]);
+    setFileList(e.target.files);
     // console.log('# fileList : ', fileList);
     // console.log('# currentImageList : ', currentImageList);
   }, []);
 
-  const imageInput = useRef(null);
+  const HandleNext = () => {
+    sessionStorage.setItem(
+      'ReviewData',
+      JSON.stringify({
+        reviewImages: fileList,
+      })
+    );
+  };
+
+  useEffect(
+    () => console.log('fileList', fileList, 'imageList', imageList),
+    [fileList, imageList]
+  );
 
   return (
     <Main meta={<Meta title="Cakestation Review" description="리뷰 맛보기" />}>
@@ -42,10 +66,15 @@ const AddPictures = () => {
         title={'리뷰 사진'}
         subtitle={'케이크 디자인이 잘 보이는 사진을 선택해 주세요.'}
         nextText={'다음'}
-        nextFunc={() => {}}
+        nextFunc={HandleNext}
         nextLink={`/reviews/write/order/`}
       >
         <div className="w-85">
+          <div className={'w-100 text-end mb-60'}>
+            <Typography category={'Bd2'} color={'cakeLemon_800'}>
+              ({fileList.length}/10)
+            </Typography>
+          </div>
           <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
             {imageList[0]?.map((item: string, index: number) => {
               console.log('item', item);
@@ -57,12 +86,14 @@ const AddPictures = () => {
               );
             })}
             <SwiperSlide>
+              <StyledLabel htmlFor={'upload-image'} />
               <input
+                id={'upload-image'}
                 type="file"
                 accept={'image/*'}
                 name="image"
                 multiple
-                // hidden
+                hidden
                 ref={imageInput}
                 onChange={onChangeImages}
               />

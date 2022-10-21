ccb1358f551f8a4bf5283bbde7cfb62a1f765330
danbom <bear0369@naver.com>
2022-10-21 15:28:35 +0900

    feat: add bottom sheet and react-kakao-maps-sdk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,16 @@ import '../styles/global.css';
 import '../styles/fonts.css';
 
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Script
+      src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&libraries=services,clusterer&autoload=false`}
+      strategy="beforeInteractive"
+    />
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;

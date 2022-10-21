7175e86668912f4c4d0839b5e98e3511baf02b22
danbom <bear0369@naver.com>
2022-10-21 16:08:07 +0900

    feat: add station marker and marker event

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,9 @@ import '../styles/global.css';
 import '../styles/fonts.css';
 
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Script
-      src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&libraries=services,clusterer&autoload=false`}
-      strategy="beforeInteractive"
-    />
-    <Component {...pageProps} />
-  </>
+  <Component {...pageProps} />
 );
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import Script from 'next/script';
+import React, { useEffect, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Sheet from 'react-modal-sheet';
 import styled from 'styled-components';
@@ -7,6 +8,7 @@ import SearchBar from '@/components/common/searchbar';
 import { Header } from '@/layouts/Header';
 import { Meta } from '@/layouts/Meta';
 import Navigation from '@/layouts/Navigation';
+import subways from '@/lib/전체지하철역.json';
 import { Main } from '@/templates/Main';
 
 // const MapContainer = styled.div`
@@ -46,31 +48,42 @@ const CustomSheet = styled(Sheet)`
   }
 `;
 
+let isAlreadyLoaded = false;
+
 const Index = () => {
   // TODO: any 쓰면 안됨~~ 변경 필요~~
 
-  // const [data, setData] = useState<Array<any>>([
-  //   { 위도: '37.5666805', 경도: '126.9784147' },
-  // ]);
+  const [loaded, setLoaded] = useState(isAlreadyLoaded);
+  const [positions, setPositions] = useState<Array<any>>([
+    { 위도: '37.5666805', 경도: '126.9784147' },
+  ]);
   const [isSheetOpen, setSheetOpen] = useState(false);
-  const [isMarkerOpen, setIsMarkerOpen] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       // const response = await axios.get(
-  //       //   process.env.NEXT_PUBLIC_LINE1_API || ''
-  //       // );
-  //       setData(subways);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = await axios.get(
+        //   process.env.NEXT_PUBLIC_LINE1_API || ''
+        // );
+        setPositions(subways);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <Main meta={<Meta title="Cakestation Map" description="지도 맛보기" />}>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`}
+        onLoad={() => {
+          kakao.maps.load(() => {
+            isAlreadyLoaded = true;
+            setLoaded(true);
+          });
+        }} // 동적으로 로드
+      />
       <Header style={'bar'}>
         <SearchBar
           placeholder="가게와 가까운 지하철 역 검색"
@@ -78,20 +91,36 @@ const Index = () => {
         />
       </Header>
       <Absolute>
-        <Map // 지도를 표시할 Container
-          center={{
-            // 지도의 중심좌표
-            lat: 33.450701,
-            lng: 126.570667,
-          }}
-          style={{
-            // 지도의 크기
-            width: '100%',
-            height: '100vh',
-          }}
-          level={3} // 지도의 확대 레벨
-        >
-          <MapMarker // 인포윈도우를 생성하고 지도에 표시합니다
+        {loaded && (
+          <Map // 지도를 표시할 Container
+            center={{
+              // 지도의 중심좌표
+              lat: 33.450701,
+              lng: 126.570667,
+            }}
+            style={{
+              // 지도의 크기
+              width: '100%',
+              height: '100vh',
+            }}
+            level={3} // 지도의 확대 레벨
+          >
+            {positions.map((position, index) => (
+              <MapMarker
+                key={index}
+                position={{ lat: position.위도, lng: position.경도 }} // 마커를 표시할 위치
+                image={{
+                  src: '/assets/images/icons/pin.svg', // 마커이미지의 주소입니다
+                  size: {
+                    width: 27,
+                    height: 27,
+                  }, // 마커이미지의 크기입니다
+                }}
+                title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                onClick={() => setSheetOpen(true)}
+              />
+            ))}
+            {/* <MapMarker // 인포윈도우를 생성하고 지도에 표시합니다
             position={{
               // 인포윈도우가 표시될 위치입니다
               lat: 33.450701,
@@ -99,31 +128,11 @@ const Index = () => {
             }}
             clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
             onClick={() => setSheetOpen(true)}
-          >
+          > */}
             {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
             {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
-            {isMarkerOpen && (
-              <div style={{ minWidth: '150px' }}>
-                <img
-                  alt="close"
-                  width="14"
-                  height="13"
-                  src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-                  style={{
-                    position: 'absolute',
-                    right: '5px',
-                    top: '5px',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => setIsMarkerOpen(false)}
-                />
-                <div style={{ padding: '5px', color: '#000' }}>
-                  Hello World!
-                </div>
-              </div>
-            )}
-          </MapMarker>
-        </Map>
+          </Map>
+        )}
       </Absolute>
       <button onClick={() => setSheetOpen(true)}>Open sheet</button>
 

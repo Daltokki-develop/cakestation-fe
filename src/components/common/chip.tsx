41e2427c62b72d987de3b876c3e35d0f9075bcfb
mineunyoung <bear0369@naver.com>
2022-11-01 15:51:06 +0900

    feat: 칩 공통 컴포넌트 개발

--- src/components/common/chip.tsx
+++ src/components/common/chip.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import palette from '@/styles/palette';
@@ -6,13 +6,17 @@ import palette from '@/styles/palette';
 import Typography from './typography';
 
 interface IChipProps {
-  value?: string;
+  currentValue?: string;
   options: Array<string>;
   onChange?: any;
 }
 
-const StyledLabel = styled.label`
+const StyledLabel = styled.div`
   display: flex;
+  position: relative;
+  width: 70px;
+  height: 32px;
+  z-index: 2;
 
   & + & {
     margin-top: 0.5rem;
@@ -25,58 +29,94 @@ const HiddenSelect = styled.select<IChipProps>`
 
 const StyledChip = styled.div`
   /* Auto layout */
-
+  position: absolute;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 6px;
-
   width: 70px;
-  height: 32px;
 
   background: ${palette.black};
   color: ${palette.white};
-  border-radius: 32px;
 
   cursor: pointer;
+
+  img {
+    margin-left: 6px;
+  }
 `;
 
-const StyledChipOpen = styled.div`
-  width: 70px;
-  height: 80px;
+const StyledChipClosed = styled(StyledChip)`
+  flex-direction: row;
+  align-items: center;
 
-  background: ${palette.black};
-  color: ${palette.white};
+  text-indent: 11px;
+
+  height: 30px;
+  border-radius: 32px;
+
+  &:hover {
+    background: #000000;
+  }
+`;
+
+const StyledChipOpen = styled(StyledChip)`
+  height: fit-content;
+  padding-bottom: 5px;
+  flex-direction: column;
   border-radius: 13px;
 
-  cursor: pointer;
+  img {
+    transform: rotate(180deg);
+  }
+`;
+
+const ChipItem = styled.div`
+  text-indent: 11px;
+  padding: 4px 0;
+  height: 20px;
 `;
 
 const Chip = (props: IChipProps) => {
+  const { currentValue, options, onChange } = props;
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(currentValue || options[0]);
+  const [filterOptions, setFilterOptions] = useState(options);
 
   const HandleOpen = useCallback(() => {
-    setOpen(!open);
-    console.log('핸들 오픈!');
+    setOpen(true);
   }, [open]);
 
+  const HandleValue = useCallback(
+    (index: number) => {
+      setOpen(false);
+      if (index > -1) {
+        setValue(filterOptions[index]);
+        onChange(filterOptions[index]);
+      }
+    },
+    [value, filterOptions]
+  );
+
+  useEffect(() => {
+    setFilterOptions(options.filter((option) => option !== value));
+  }, [value]);
+
   return (
-    <StyledLabel onClick={HandleOpen}>
-      <HiddenSelect
-        value={props.value}
-        options={props.options}
-        onChange={props.onChange}
-      />
+    <StyledLabel>
+      <HiddenSelect value={value} options={options} onChange={onChange} />
       {!open ? (
-        <StyledChip>
-          <Typography category={'Bd8'}>{props.value}</Typography>
+        <StyledChipClosed onClick={HandleOpen}>
+          <Typography category={'Bd8'}>{value}</Typography>
           <img src={'/assets/images/icons/chip_arrow.svg'} alt={'arrow'} />
-        </StyledChip>
+        </StyledChipClosed>
       ) : (
         <StyledChipOpen>
-          {props.options.map((option, index) => (
-            <option key={index}>{option}</option>
+          <ChipItem onClick={() => HandleValue(-1)}>
+            <Typography category={'Bd8'}>{value}</Typography>
+            <img src={'/assets/images/icons/chip_arrow.svg'} alt={'arrow'} />
+          </ChipItem>
+          {filterOptions.map((option, index) => (
+            <ChipItem onClick={() => HandleValue(index)} key={index}>
+              <Typography category={'Bd8'}>{option}</Typography>
+            </ChipItem>
           ))}
         </StyledChipOpen>
       )}

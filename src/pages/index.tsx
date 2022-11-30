e06ddd3ee4582bacd9e05b5180997abfaa796271
danbom <bear0369@naver.com>
2022-11-30 17:57:53 +0900

    feat: 메인 화면 검색 토글 추가, 최근 검색 기록 조회 기능 추가, 역별/이름별 검색 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/naming-convention */
+import Image from 'next/image';
 import Script from 'next/script';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Sheet from 'react-modal-sheet';
 import styled from 'styled-components';
 
+import Chip from '@/components/common/chip';
+import Divider from '@/components/common/divider';
 import ItemCard from '@/components/common/itemcard';
-import SearchBar from '@/components/common/searchbar';
 import Typography from '@/components/common/typography';
 import { Header } from '@/layouts/Header';
 import { Meta } from '@/layouts/Meta';
 import Navigation from '@/layouts/Navigation';
-import { AXIOS_GET } from '@/lib/commonFunction';
+import {
+  AXIOS_GET,
+  getSessionSearchHistory,
+  setSessionSearchHistory,
+} from '@/lib/commonFunction';
 import { BASE_URL } from '@/lib/ConstantURL';
 import subways from '@/lib/전체지하철역.json';
 import palette from '@/styles/palette';
 import { Main } from '@/templates/Main';
-
-// const MapContainer = styled.div`
-//   /* aspect-ratio: 4 / 3; */
-//   width: 100%;
-//   height: 100vh;
-// `;
 
 const Absolute = styled.div`
   position: absolute;
@@ -29,6 +31,7 @@ const Absolute = styled.div`
   width: 100%;
   height: 100vh;
   z-index: 1;
+  overflow: hidden;
 `;
 
 const CustomSheet = styled(Sheet)`
@@ -71,8 +74,6 @@ const SheetTitle = styled.div`
 const SheetContent = styled.div`
   /* padding: 0 20px; */
 `;
-
-// const StationTag = styled.div`
 //   padding: 8px 10px;
 //   width: max-content;
 //   height: max-content;
@@ -99,30 +100,145 @@ const SetMyLocationButton = styled.div`
   cursor: pointer;
 `;
 
-const Chip = styled.div`
-  width: 70px;
-  height: 32px;
-  background-color: ${palette.black};
-  color: ${palette.white};
-  border-radius: 32px;
+const StyledSearchBar = styled.div`
+  /* width: 100%; */
+  /* height: 3rem; */
+  background: ${palette.white};
+  border: 0.125rem solid ${palette.black};
+  border-radius: 0.5rem;
   display: flex;
   align-items: center;
-  justify-content: center;
+  padding: 0.5rem;
+
+  input {
+    margin-left: 0.75rem;
+    width: calc(100% - 2.5rem - 0.75rem);
+    height: 100%;
+    background-color: transparent;
+    border: none;
+    font-family: 'Pretendard-Regular';
+    font-size: 1rem;
+
+    :focus-visible {
+      outline: none;
+    }
+  }
 `;
 
+const Toggle = styled.div`
+  width: 80px;
+  height: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-indent: 11px;
+
+  cursor: pointer;
+`;
+
+const SearchBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${palette.white};
+  z-index: 2;
+  padding-top: 7.5rem;
+`;
+
+const SearchResultContainer = styled.div`
+  width: 100%;
+`;
+
+const SearchResultHeader = styled.div`
+  height: 54px;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+`;
+
+const SearchResultContent = styled.div`
+  height: calc(100vh - 5.1875rem - 7.5rem - 0.375rem - 54px);
+  overflow-y: scroll;
+  margin-bottom: 5.1875rem;
+
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+`;
+
+const NoResultContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  .no-result-image {
+    margin-bottom: 4px;
+  }
+`;
+
+const RecentSearchItem = styled.div`
+  padding: 15.5px 16px;
+  display: flex;
+  align-items: center;
+
+  .list-icon {
+    width: 24px;
+    height: 24px;
+    margin-right: 12px;
+  }
+`;
+
+// const  = styled.div`
+//   width: 70px;
+//   height: 32px;
+//   background-color: ${palette.black};
+//   color: ${palette.white};
+//   border-radius: 32px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
 let isAlreadyLoaded = false;
+
+const CATEGORY_LIST = [
+  {
+    label: '지하철역',
+    value: 'station',
+    params: 'stationName',
+  },
+  {
+    label: '가게',
+    value: 'name',
+    params: 'storeName',
+  },
+];
 
 const Index = () => {
   // TODO: any 쓰면 안됨~~ 변경 필요~~
 
+  /* useState */
+  // 지도가 로드되었는 지 확인하는 변수
   const [loaded, setLoaded] = useState(isAlreadyLoaded);
+
+  // 지하철역 마커들 찍을 위치 배열
   const [positions, setPositions] = useState<Array<any>>([
     { 위도: '37.5666805', 경도: '126.9784147' },
   ]);
+
+  // 지도의 중심 좌표를 담을 변수
   const [center, setCenter] = useState({
     lat: 33.450701,
     lng: 126.570667,
   });
+
+  // 사용자의 현재 위치 좌표를 담을 변수
   const [now, setNow] = useState({
     center: {
       lat: 33.450701,
@@ -131,13 +247,103 @@ const Index = () => {
     errMsg: '',
     isLoading: true,
   });
+
   const [selected, setSelected] = useState('');
 
   const [isSheetOpen, setSheetOpen] = useState(false);
 
+  const [searchMode, setSearchMode] = useState(false);
+
+  const [searchComplete, setSearchComplete] = useState(false);
+
+  const [searchHistoryList, setSearchHistoryList] = useState<
+    Array<{ category: string; keyword: string }>
+  >([]);
+
   const [resultList, setResultList] = useState<Array<any>>([]);
 
+  const [category, setCategory] = useState<any>({});
+
+  const [keyword, setKeyword] = useState('');
+
   const mapRef = useRef();
+
+  const HandleToggle = useCallback(() => {
+    if (category === CATEGORY_LIST[0]) {
+      setCategory(CATEGORY_LIST[1]);
+    } else if (category === CATEGORY_LIST[1]) {
+      setCategory(CATEGORY_LIST[0]);
+    }
+  }, [category]);
+
+  const HandleInputChange = useCallback((event: any) => {
+    setSearchComplete(false);
+    if (event.target.value) {
+      setSearchMode(true);
+    } else {
+      setSearchMode(false);
+    }
+    setKeyword(event.target.value);
+    setResultList([]);
+  }, []);
+
+  const HandleFetchData = useCallback(async () => {
+    const { value, params } = category;
+    const response = await AXIOS_GET(
+      `${BASE_URL}/api/stores/search/${value}?${params}=${keyword}`
+    );
+
+    setResultList(response?.data.result);
+  }, [category, keyword]);
+
+  const HandleSearchHistory = useCallback(() => {
+    let searchHistory = getSessionSearchHistory();
+    if (searchHistory) {
+      const arr = Object.keys(searchHistory).map((item) => parseInt(item, 10));
+      const maxNum = Math.max(...arr) + 1;
+      searchHistory[
+        maxNum
+      ] = `${category.value} ||| ${category.params} ||| ${keyword}`;
+    } else {
+      searchHistory = {
+        0: `${category.value} ||| ${category.params} ||| ${keyword}`,
+      };
+    }
+
+    const _searchHistoryList:
+      | ((
+          prevState: {
+            category: string;
+            keyword: string;
+          }[]
+        ) => { category: string; keyword: string }[])
+      | { category: any; keyword: any }[] = [];
+    Object.keys(searchHistory).forEach((index) => {
+      const _split = searchHistory[index].split(' ||| ');
+      const _category = _split[0];
+      const _keyword = _split[2];
+      if (_keyword) {
+        _searchHistoryList.push({ category: _category, keyword: _keyword });
+      }
+    });
+
+    setSearchHistoryList(_searchHistoryList);
+    setSessionSearchHistory(searchHistory);
+  }, [category.params, category.value, keyword]);
+
+  const HandleInputKeyDown = useCallback(
+    (event: any) => {
+      if (event.key === 'Enter') {
+        setSearchComplete(true);
+        HandleFetchData();
+
+        if (keyword) {
+          HandleSearchHistory();
+        }
+      }
+    },
+    [HandleFetchData, HandleSearchHistory, keyword]
+  );
 
   const handleMoveLocation = () => {
     const map = mapRef.current;
@@ -204,8 +410,14 @@ const Index = () => {
     setCenter(now.center);
   }, [now]);
 
+  useEffect(() => {
+    HandleSearchHistory();
+    setCategory(CATEGORY_LIST[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <Main meta={<Meta title="Cakestation Map" description="지도 맛보기" />}>
+    <Main meta={<Meta title="Cakestation Map" description="메인 화면" />}>
       <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`}
         onLoad={() => {
@@ -216,13 +428,106 @@ const Index = () => {
         }} // 동적으로 로드
       />
       <Header style={'bar'}>
-        <SearchBar
-          placeholder="가게와 가까운 지하철 역 검색"
-          onChange={() => {}}
-        />
+        <StyledSearchBar>
+          <Toggle onClick={HandleToggle}>
+            <Typography category={'Bd8'}>{category?.label}</Typography>
+            <Image
+              src={'/assets/images/icons/up.svg'}
+              width={10}
+              height={6}
+              alt={'arrow'}
+            />
+          </Toggle>
+          <input
+            placeholder="지하철역, 가게이름 검색"
+            onChange={HandleInputChange}
+            onKeyDown={HandleInputKeyDown}
+          />
+        </StyledSearchBar>
       </Header>
       <Absolute>
-        {loaded && (
+        {searchMode && (
+          <SearchBackground>
+            <SearchResultContainer>
+              {searchComplete && (
+                <>
+                  <SearchResultHeader>
+                    <Typography category="Bd2" color="cakeLavender_800">
+                      &apos;{keyword}&apos;&nbsp;
+                    </Typography>
+                    <Typography category="Bd2">검색 결과</Typography>
+                  </SearchResultHeader>
+                  <Divider size={'large'} />
+                  <SearchResultContent>
+                    {resultList.length > 0 ? (
+                      <>
+                        {resultList.map((result: any, index: React.Key) => {
+                          const { address, name, score, reviewNum } = result; // storeId,
+                          return (
+                            <ItemCard
+                              key={index}
+                              title={name}
+                              rate={score || 0}
+                              count={reviewNum || 0}
+                              distance={address}
+                              pictures={[]}
+                              // onClick={() => GoReviewWrite(storeId)}
+                            />
+                          );
+                        })}
+                      </>
+                    ) : (
+                      <NoResultContainer>
+                        <div className="no-result-image">
+                          <Image
+                            src="/assets/images/icons/no_result.svg"
+                            alt="NO RESULT"
+                            width={140}
+                            height={140}
+                          />
+                        </div>
+                        <Typography category={'Bd2'} color={'grey_400'}>
+                          앗!
+                          <br />
+                        </Typography>
+                        <Typography category={'Bd7'} color={'grey_400'}>
+                          검색 결과가 없습니다.
+                        </Typography>
+                      </NoResultContainer>
+                    )}
+                  </SearchResultContent>
+                </>
+              )}
+            </SearchResultContainer>
+            {!searchComplete && (
+              <SearchResultContainer>
+                <SearchResultHeader>
+                  <Typography category="Bd2">최근 검색어</Typography>
+                </SearchResultHeader>
+                <Divider size={'large'} />
+                <SearchResultContent>
+                  {searchHistoryList?.map((item, index) => {
+                    return (
+                      <RecentSearchItem key={index}>
+                        <div className="list-icon">
+                          <Image
+                            src={`/assets/images/icons/${item.category}.svg`}
+                            width={24}
+                            height={24}
+                            alt={item.category}
+                          />
+                        </div>
+                        <Typography category="Bd2">{item.keyword}</Typography>
+                      </RecentSearchItem>
+                    );
+                  })}
+                </SearchResultContent>
+              </SearchResultContainer>
+            )}
+            {/* <div className="empty-container" /> */}
+          </SearchBackground>
+        )}
+        {loaded ? (
           <Map // 지도를 표시할 Container
             center={center}
             style={{
@@ -278,21 +583,16 @@ const Index = () => {
               }}
               title={'현재 위치'} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             />
-            {/* <MapMarker // 인포윈도우를 생성하고 지도에 표시합니다
-            position={{
-              // 인포윈도우가 표시될 위치입니다
-              lat: 33.450701,
-              lng: 126.570667,
-            }}
-            clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-            onClick={() => setSheetOpen(true)}
-          > */}
-            {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
-            {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
+            {!searchMode && (
+              <SetMyLocationButton onClick={setCurrentLocation}>
+                <img src={'/assets/images/icons/spot.svg'} alt={'spot'} />
+              </SetMyLocationButton>
+            )}
           </Map>
+        ) : (
+          <div>로딩</div>
         )}
       </Absolute>
-      지도 로딩중...
       <CustomSheet isOpen={isSheetOpen} onClose={() => setSheetOpen(false)}>
         <CustomSheet.Container>
           <CustomSheet.Header />
@@ -318,32 +618,29 @@ const Index = () => {
               </div> */}
               <div className="flex items-center contents-space-between">
                 <Typography category="H3">이 근처 케이크집 리뷰</Typography>
-                <Chip>
-                  <Typography category="Bd8">최신순</Typography>
-                  <img
-                    className="ml-6"
-                    src="/assets/images/icons/chip_arrow.svg"
-                    alt="arrow"
-                  />
-                </Chip>
+                <Chip options={['최신순', '인기순']} />
               </div>
             </SheetHeader>
             <SheetContent>
               <div>
-                {resultList.map((result: any, index: React.Key) => {
-                  const { address, name, score, reviewNum } = result; // storeId,
-                  return (
-                    <ItemCard
-                      key={index}
-                      title={name}
-                      rate={score || 0}
-                      count={reviewNum || 0}
-                      distance={address}
-                      pictures={[]}
-                      // onClick={() => GoReviewWrite(storeId)}
-                    />
-                  );
-                })}
+                {resultList.length > 0 ? (
+                  resultList.map((result: any, index: React.Key) => {
+                    const { address, name, score, reviewNum } = result; // storeId,
+                    return (
+                      <ItemCard
+                        key={index}
+                        title={name}
+                        rate={score || 0}
+                        count={reviewNum || 0}
+                        distance={address}
+                        pictures={[]}
+                        // onClick={() => GoReviewWrite(storeId)}
+                      />
+                    );
+                  })
+                ) : (
+                  <div>검색결과 업슴</div>
+                )}
               </div>
             </SheetContent>
           </CustomSheet.Content>
@@ -351,9 +648,7 @@ const Index = () => {
 
         <CustomSheet.Backdrop />
       </CustomSheet>
-      <SetMyLocationButton onClick={setCurrentLocation}>
-        <img src={'/assets/images/icons/spot.svg'} alt={'spot'} />
-      </SetMyLocationButton>
+
       <Navigation type={'default'} />
       {/* <Navigation type={'item'} /> */}
     </Main>

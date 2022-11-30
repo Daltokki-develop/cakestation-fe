9c5b0ae6d4d12270415cba559a120a673a8c650c
danbom <bear0369@naver.com>
2022-11-30 19:53:07 +0900

    feat: 가게 검색 페이지 로딩, 검색 결과 로직 수정

--- src/layouts/StoreSearch.tsx
+++ src/layouts/StoreSearch.tsx
@@ -1,20 +1,44 @@
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
+import styled from 'styled-components';
 
 import Button from '@/components/common/button';
 import ItemCard from '@/components/common/itemcard';
 import SearchBar from '@/components/common/searchbar';
 import Section from '@/components/common/section';
 import Typography from '@/components/common/typography';
+import NoResult from '@/components/NoResult';
 import { Header } from '@/layouts/Header';
 import Navigation from '@/layouts/Navigation';
 import { AXIOS_GET } from '@/lib/commonFunction';
 import { BASE_URL } from '@/lib/ConstantURL';
+import palette from '@/styles/palette';
 
 interface IStoreSearchProps {
   title: string;
   sub: string;
 }
+
+const LoadingContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${palette.black};
+  opacity: 0.5;
+
+  z-index: 105;
+
+  .loading-icon {
+    margin-bottom: 4px;
+  }
+`;
 
 const StoreSearchHeader = (props: IStoreSearchProps) => {
   return (
@@ -25,24 +49,6 @@ const StoreSearchHeader = (props: IStoreSearchProps) => {
       <div className="mb-18">
         <Typography category={'Bd2'}>{props.sub}</Typography>
       </div>
-    </div>
-  );
-};
-
-const NoResult = () => {
-  return (
-    <div className="column items-center">
-      <img
-        src="/assets/images/icons/no_result.svg"
-        alt="NO RESULT"
-        width={200}
-        className="mt-20 mb-33"
-      />
-      <Typography category={'Bd7'} color={'grey_400'}>
-        앗!
-        <br />
-        검색 결과가 없습니다.
-      </Typography>
     </div>
   );
 };
@@ -64,35 +70,42 @@ const SearchButton = (FetchResultList: (() => void) | undefined) => {
   );
 };
 
-const FetchAllResultList = async (setResultList: any) => {
-  try {
-    const response = await AXIOS_GET(`${BASE_URL}/api/stores/all`);
-    setResultList(response?.data.result);
-  } catch (e) {
-    console.error(e);
-    setResultList({});
-  }
-};
-
-const FetchResultList = async (keyword: string, setResultList: any) => {
-  try {
-    const response = await AXIOS_GET(
-      `${BASE_URL}/api/stores/search/name?storeName=${keyword}`
-    );
-    setResultList(response?.data.result);
-  } catch (e) {
-    console.error(e);
-    setResultList({});
-  }
-};
-
 const StoreSearch = (props: IStoreSearchProps) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [searchComplete, setSearchComplete] = useState<boolean>(false);
   const [keyword, setKeyword] = useState<string>('');
   const [resultList, setResultList] = useState<Array<any>>([]);
   const router = useRouter();
 
+  const FetchAllResultList = async () => {
+    try {
+      setLoading(true);
+      const response = await AXIOS_GET(`${BASE_URL}/api/stores/all`);
+      setLoading(false);
+      setResultList(response?.data.result);
+    } catch (e) {
+      setResultList([]);
+    }
+  };
+
+  const FetchResultList = async () => {
+    try {
+      setLoading(true);
+      setSearchComplete(true);
+      const response = await AXIOS_GET(
+        `${BASE_URL}/api/stores/search/name?storeName=${keyword}`
+      );
+      setLoading(false);
+      setResultList(response?.data.result);
+    } catch (e) {
+      setResultList([]);
+    }
+  };
+
   const FetchResultListwithKey = async (e: any) => {
-    if (e.key === 'Enter') await FetchResultList(keyword, setResultList);
+    if (e.key === 'Enter') {
+      await FetchResultList();
+    }
   };
 
   const GoReviewWrite = async (storeId: string) => {
@@ -102,20 +115,35 @@ const StoreSearch = (props: IStoreSearchProps) => {
     await router.push('/reviews/write/addpictures/');
   };
 
+  const HandleInputChange = useCallback((event: any) => {
+    setSearchComplete(false);
+    setKeyword(event.target.value);
+  }, []);
+
   useEffect(() => {
-    FetchAllResultList(setResultList);
+    FetchAllResultList();
     // sessionStorage.setItem('ReviewData', JSON.stringify(''));
   }, []);
 
   return (
     <>
+      {loading && (
+        <LoadingContainer>
+          <div className="loading-icon">
+            <ClipLoader color={palette.white} loading={loading} size={30} />
+          </div>
+          <Typography category="Bd5" color="white">
+            가게 목록을 불러오고 있습니다...
+          </Typography>
+        </LoadingContainer>
+      )}
       <Header style={'text'}>{props.title}</Header>
       <Section padding={'11rem'}>
         <div className="w-85">
           {StoreSearchHeader(props)}
           <SearchBar
             placeholder={'가게 이름 검색'}
-            onChange={(e: any) => setKeyword(e.target.value)}
+            onChange={(e: Object) => HandleInputChange(e)}
             onKeyPress={FetchResultListwithKey}
           />
           <div className="column mt-20 text-center">
@@ -138,11 +166,10 @@ const StoreSearch = (props: IStoreSearchProps) => {
                 })}
               </>
             ) : (
-              NoResult()
+              searchComplete && <NoResult />
             )}
           </div>
-          {keyword &&
-            SearchButton(() => FetchResultList(keyword, setResultList))}
+          {keyword && SearchButton(() => FetchResultList())}
         </div>
       </Section>
       <Navigation type={'default'} />

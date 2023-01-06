5b92424f22790aec8dec085b593fd455df5c9547
danbom <bear0369@naver.com>
2023-01-07 03:40:29 +0900

    feat: axios 패치 메서드 추가

--- src/lib/commonFunction.ts
+++ src/lib/commonFunction.ts
@@ -46,6 +46,7 @@ const getAuthHeader = (method: string) => {
   switch (method) {
     case 'GET':
     case 'DELETE':
+    case 'PATCH':
       break;
 
     case 'POST':
@@ -224,5 +225,40 @@ export const AXIOS_POST_FORM = (
         console.log('# 에러: ', error.message);
       }
       console.log('# config: ', error.config);
+    });
+};
+
+// 헤더가 세팅된 axios PATCH 요청
+export const AXIOS_PATCH = (url: string, timeOut: number = TIME_OUT) => {
+  console.log(`====== AXIOS_PATCH =======`);
+  console.log('url :: ', url);
+
+  const header = getAuthHeader('PATCH');
+  console.log('header :: ', header);
+  console.log(`========================`);
+
+  return axios
+    .patch(url, {
+      headers: header,
+      timeout: timeOut,
+    })
+    .catch(function (error) {
+      console.log(`========= ERROR ========`);
+      if (error.response) {
+        // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+        console.log('# 데이터 :: ', error.response.data);
+        console.log('# 상태 :: ', error.response.status);
+        console.log('# 헤더 :: ', error.response.headers);
+      } else if (error.request) {
+        // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+        // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+        // Node.js의 http.ClientRequest 인스턴스입니다.
+        console.log(error.request);
+      } else {
+        // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+        console.log('Error', error.message);
+      }
+      console.log('# config :: ', error.config);
+      console.log(`========================`);
     });
 };

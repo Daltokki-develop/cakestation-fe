09fead97e78088bebb705f7749004abe802cd40d
danbom <bear0369@naver.com>
2023-01-06 20:27:47 +0900

    chore: 로그인 화면 정렬

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
 
 import Button from '@/components/common/button';
 import Section from '@/components/common/section';
@@ -8,36 +9,47 @@ import { Meta } from '@/layouts/Meta';
 import { KAKAO_URL } from '@/lib/ConstantURL';
 import { Main } from '@/templates/Main';
 
+const FullHeightContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const Login = () => {
   return (
     <Main meta={<Meta title="Cakestation Login" description="login" />}>
       <Section>
         <div className="w-85">
-          <div className="mt-143">
-            <Typography category={'H2'}>케이크 가게 찾기의 종착역</Typography>
-          </div>
-          <div className="mt-6">
-            <Typography category={'H1'}>CAKE STATION</Typography>
-          </div>
-          <div className="mt-48">
-            <img src="/assets/images/landing-logo.gif" alt="landing-logo" />
-          </div>
-          <div className="mt-48">
-            <Link href={KAKAO_URL} className="mt-48">
-              <a>
-                <Button size={'medium'} category={'primary'}>
-                  <img
-                    className="mr-7_5 w-21 h-19_3"
-                    src="/assets/images/icons/kakao.svg"
-                    alt="kakaoTalk"
-                  />
-                  <Typography category={'Bd5'}>
-                    카카오톡으로 시작하기
-                  </Typography>
-                </Button>
-              </a>
-            </Link>
-          </div>
+          <FullHeightContainer>
+            <div>
+              <Typography category={'H2'}>케이크 가게 찾기의 종착역</Typography>
+            </div>
+            <div className="mt-6">
+              <Typography category={'H1'}>
+                <strong>CAKE STATION</strong>
+              </Typography>
+            </div>
+            <div className="mt-48">
+              <img src="/assets/images/landing-logo.gif" alt="landing-logo" />
+            </div>
+            <div className="mt-48">
+              <Link href={KAKAO_URL} className="mt-48">
+                <a>
+                  <Button size={'medium'} category={'primary'}>
+                    <img
+                      className="mr-7_5 w-21 h-19_3"
+                      src="/assets/images/icons/kakao.svg"
+                      alt="kakaoTalk"
+                    />
+                    <Typography category={'Bd5'}>
+                      카카오톡으로 시작하기
+                    </Typography>
+                  </Button>
+                </a>
+              </Link>
+            </div>
+          </FullHeightContainer>
         </div>
       </Section>
     </Main>

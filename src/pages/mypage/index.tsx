e182961032b2ae42bbc1d37258ac657882f6b7e4
danbom <bear0369@naver.com>
2023-01-07 03:41:17 +0900

    feat: 링크 연결, 닉네임 변경 api 연결 등 마이페이지 업데이트

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,24 +1,54 @@
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Divider from '@/components/common/divider';
 import Section from '@/components/common/section';
 import Typography from '@/components/common/typography';
+import LoadingContainer from '@/components/loading';
 import { Header } from '@/layouts/Header';
 import { Meta } from '@/layouts/Meta';
 import Navigation from '@/layouts/Navigation';
-import { AXIOS_GET } from '@/lib/commonFunction';
+import { AXIOS_GET, AXIOS_PATCH } from '@/lib/commonFunction';
 import { BASE_URL } from '@/lib/ConstantURL';
-import palette from '@/styles/palette';
 import { Main } from '@/templates/Main';
 
-const VerticalBar = styled.div`
-  width: 2px;
-  height: 55.5px;
-  background-color: ${palette.grey_300};
+const Absolute = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
+  overflow: hidden;
+`;
+
+const ProfileItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 24px 0;
+  padding: 0 18px;
+`;
+
+const LinkItem = styled.div`
+  height: 68px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 18px;
+  cursor: pointer;
+`;
+
+const ResetButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-left: 6px;
 `;
 
 const MyPage = () => {
+  const [loading, setLoading] = useState(false);
   const [myData, setMyData] = useState({
     randomNumber: 1,
     nickName: null,
@@ -33,6 +63,19 @@ const MyPage = () => {
       if (response?.status === 200) {
         const fetchData = response?.data.result;
         setMyData(fetchData);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const ResetNickname = async () => {
+    try {
+      const response = await AXIOS_PATCH(`${BASE_URL}/api/nickname`);
+      if (response?.status === 200) {
+        const fetchData = response?.data.result;
+        setMyData(fetchData);
       }
     } catch (e) {
       console.error(e);
@@ -40,18 +83,18 @@ const MyPage = () => {
   };
 
   useEffect(() => {
+    setLoading(true);
     FetchMyData();
   }, []);
 
-  const { randomNumber, nickName, reviewCount, reviewImageCount, likeCount } =
-    myData;
+  const { randomNumber, nickName, reviewCount, likeCount } = myData;
 
   return (
     <Main meta={<Meta title="Cakestation Review" description="마이페이지" />}>
       <Header style={'text'}>마이페이지</Header>
       <Section>
         <div className="w-100 column">
-          <div className="flex items-center mt-16 pl-18">
+          <ProfileItem>
             <img
               className={'w-74 h-74 mr-18'}
               src={`/assets/images/profile/${randomNumber}.svg`}
@@ -60,69 +103,63 @@ const MyPage = () => {
             <Typography category={'H1'}>
               {nickName || '비어있는 닉네임'}
             </Typography>
-          </div>
-          <div className="mt-34 pl-18 pr-18 column">
-            <Typography category={'Bd4'}>활동</Typography>
-            <div className="flex contents-space-between mt-5 mb-22 pl-86 pr-86">
-              <div className="column">
+            <ResetButton onClick={ResetNickname}>
+              <img src={'/assets/images/icons/reset.svg'} alt={'Reset'} />
+            </ResetButton>
+          </ProfileItem>
+
+          {/* <Divider size={'large'} /> */}
+          <Link href="/likes">
+            <LinkItem>
+              <Typography category={'Bd4'}>좋아요</Typography>
+              <div>
+                <Typography category={'Bd4'}>{likeCount}</Typography>
                 <img
-                  className="w-40 h-40"
-                  src={'/assets/images/icons/rate_mypage.svg'}
-                  alt="rate"
+                  className="w-12 h-12 ml-18"
+                  src={'/assets/images/icons/right_black.svg'}
+                  alt="right"
                 />
-                <Typography category={'Bd5'} color={'grey_500'}>
-                  리뷰 {reviewCount}
-                </Typography>
               </div>
-              <VerticalBar />
-              <div className="column">
-                <img
-                  className="w-40 h-40"
-                  src={'/assets/images/icons/photo_mypage.svg'}
-                  alt="photo"
-                />
-                <Typography category={'Bd5'} color={'grey_500'}>
-                  사진 {reviewImageCount}
-                </Typography>
-              </div>
-            </div>
-          </div>
+            </LinkItem>
+          </Link>
+
           <Divider size={'large'} />
-          <div className="h-68 flex items-center contents-space-between pl-18 pr-18">
-            <Typography category={'Bd4'}>좋아요</Typography>
-            <div>
-              <Typography category={'Bd4'}>{likeCount}</Typography>
+          <Link href="/mypage/myreviews">
+            <LinkItem>
+              <Typography category={'Bd4'}>내가 등록한 리뷰</Typography>
+              <div>
+                <Typography category={'Bd4'}>{reviewCount}</Typography>
+                <img
+                  className="w-12 h-12 ml-18"
+                  src={'/assets/images/icons/right_black.svg'}
+                  alt="right"
+                />
+              </div>
+            </LinkItem>
+          </Link>
+
+          <Divider size={'large'} />
+          <Link href="/mypage/setting">
+            <LinkItem>
+              <Typography category={'Bd4'}>설정</Typography>
               <img
                 className="w-12 h-12 ml-18"
                 src={'/assets/images/icons/right_black.svg'}
                 alt="right"
               />
-            </div>
-          </div>
-          <Divider size={'large'} />
-          <div className="h-68 flex items-center contents-space-between pl-18 pr-18">
-            <Typography category={'Bd4'}>내가 등록한 리뷰</Typography>
-            <div>
-              <Typography category={'Bd4'}>{reviewCount}</Typography>
-              <img
-                className="w-12 h-12 ml-18"
-                src={'/assets/images/icons/right_black.svg'}
-                alt="right"
-              />
-            </div>
-          </div>
-          <Divider size={'large'} />
-          <div className="h-68 flex items-center contents-space-between pl-18 pr-18">
-            <Typography category={'Bd4'}>설정</Typography>
-            <img
-              className="w-12 h-12 ml-18"
-              src={'/assets/images/icons/right_black.svg'}
-              alt="right"
-            />
-          </div>
+            </LinkItem>
+          </Link>
         </div>
       </Section>
 
+      {loading ? (
+        <Absolute>
+          <LoadingContainer
+            text="마이페이지 데이터 불러오는 중..."
+            loaded={!loading}
+          />
+        </Absolute>
+      ) : null}
       <Navigation type={'default'} />
     </Main>
   );

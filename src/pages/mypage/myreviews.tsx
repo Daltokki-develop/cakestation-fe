588c250c8b6be11838fc796ee493ca292fa667fb
danbom <bear0369@naver.com>
2023-01-08 23:33:56 +0900

    feat: 내가등록한리뷰 페이지 업데이트 (WIP)

--- src/pages/mypage/myreviews.tsx
+++ src/pages/mypage/myreviews.tsx
@@ -1,20 +1,28 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '@/components/common/button';
-import ItemCard from '@/components/common/itemcard';
+import ReviewCard from '@/components/common/reviewCard';
 import Section from '@/components/common/section';
 import Typography from '@/components/common/typography';
+import LoadingContainer from '@/components/loading';
 import { Header } from '@/layouts/Header';
 import { Meta } from '@/layouts/Meta';
 import Navigation from '@/layouts/Navigation';
+import { AXIOS_GET } from '@/lib/commonFunction';
+import { BASE_URL } from '@/lib/ConstantURL';
 import { Main } from '@/templates/Main';
 
-const MT16 = styled.div`
-  & + & {
-    margin-top: 1rem;
-  }
+const Absolute = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
+  overflow: hidden;
 `;
 
 const StyledEmptyContainer = styled.div`
@@ -35,7 +43,8 @@ const StyledFindBtnContainer = styled.div`
 
 const Container = styled.div`
   width: 100%;
-  height: calc(100vh - 96px - 75px);
+  height: fit-content;
+  min-height: calc(100vh - 96px - 75px);
 
   display: flex;
   flex-direction: column;
@@ -43,15 +52,33 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const MyReviews = () => {
-  const [myReviewsList, setMyReviewsList] = useState<Array<any>>();
+const CountText = styled.div`
+  width: calc(100% - 20px - 20px);
+  margin: 12px 20px;
+`;
 
-  const FetchMyReviewsList = () => {
-    setMyReviewsList([]);
+const MyReviews = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [myReviewsList, setMyReviewsList] = useState<Array<any>>();
+  const router = useRouter();
+
+  const FetchMyReviewsList = async () => {
+    try {
+      setLoading(true);
+      const response = await AXIOS_GET(
+        `${BASE_URL}/api/users/2/reviews`,
+        router
+      );
+      setLoading(false);
+      setMyReviewsList(response?.data.result);
+    } catch (error) {
+      setMyReviewsList([]);
+    }
   };
 
   useEffect(() => {
     FetchMyReviewsList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -64,18 +91,45 @@ const MyReviews = () => {
         <Header style={'text'}>내가 등록한 리뷰</Header>
         <Container>
           {myReviewsList && myReviewsList[0] ? (
-            myReviewsList.map((myReview, index) => (
-              <MT16 key={index}>
-                <ItemCard
-                  title={myReview.title}
-                  rate={myReview.rate}
-                  count={myReview.count}
-                  distance={myReview.distance}
-                  pictures={myReview.pictures}
-                  heart
-                />
-              </MT16>
-            ))
+            <>
+              <CountText>
+                <Typography category={'Bd1'} color={'black'}>
+                  전체{' '}
+                </Typography>
+                <Typography category={'Bd1'} color={'cakeLavender_600'}>
+                  {myReviewsList.length}
+                </Typography>
+              </CountText>
+
+              {myReviewsList.map((result: any, index: React.Key) => {
+                const {
+                  username,
+                  cakeNumber,
+                  score,
+                  sheetType,
+                  requestOption,
+                  reviewImages,
+                  tags,
+                  content,
+                  createdDateTime,
+                } = result;
+
+                return (
+                  <ReviewCard
+                    key={index}
+                    username={username}
+                    cakeNumber={cakeNumber}
+                    score={score}
+                    sheetType={sheetType}
+                    requestOption={requestOption}
+                    reviewImages={reviewImages}
+                    tags={tags}
+                    content={content}
+                    createdDateTime={createdDateTime}
+                  />
+                );
+              })}
+            </>
           ) : (
             <StyledEmptyContainer>
               <Typography category={'Bd3'} color={'grey_300'}>
@@ -95,6 +149,14 @@ const MyReviews = () => {
 
         <Navigation type={'default'} my />
       </Section>
+      {loading ? (
+        <Absolute>
+          <LoadingContainer
+            text="마이페이지 데이터 불러오는 중..."
+            loaded={!loading}
+          />
+        </Absolute>
+      ) : null}
     </Main>
   );
 };

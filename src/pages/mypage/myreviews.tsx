2afdd05b76aa42d0d5d47bbdb50182aebea48625
danbom <bear0369@naver.com>
2023-01-08 23:59:46 +0900

    feat: 내가등록한리뷰 페이지 로딩로직 업데이트

--- src/pages/mypage/myreviews.tsx
+++ src/pages/mypage/myreviews.tsx
@@ -1,29 +1,20 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
 import styled from 'styled-components';
 
 import Button from '@/components/common/button';
 import ReviewCard from '@/components/common/reviewCard';
 import Section from '@/components/common/section';
 import Typography from '@/components/common/typography';
-import LoadingContainer from '@/components/loading';
 import { Header } from '@/layouts/Header';
 import { Meta } from '@/layouts/Meta';
 import Navigation from '@/layouts/Navigation';
 import { AXIOS_GET } from '@/lib/commonFunction';
 import { BASE_URL } from '@/lib/ConstantURL';
+import palette from '@/styles/palette';
 import { Main } from '@/templates/Main';
-
-const Absolute = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 1;
-  overflow: hidden;
-`;
 
 const StyledEmptyContainer = styled.div`
   text-align: center;
@@ -50,6 +41,10 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  .loading-icon {
+    margin-bottom: 4px;
+  }
 `;
 
 const CountText = styled.div`
@@ -90,73 +85,86 @@ const MyReviews = () => {
       <Section padding={'8rem'}>
         <Header style={'text'}>내가 등록한 리뷰</Header>
         <Container>
-          {myReviewsList && myReviewsList[0] ? (
-            <>
-              <CountText>
-                <Typography category={'Bd1'} color={'black'}>
-                  전체{' '}
-                </Typography>
-                <Typography category={'Bd1'} color={'cakeLavender_600'}>
-                  {myReviewsList.length}
-                </Typography>
-              </CountText>
-
-              {myReviewsList.map((result: any, index: React.Key) => {
-                const {
-                  username,
-                  cakeNumber,
-                  score,
-                  sheetType,
-                  requestOption,
-                  reviewImages,
-                  tags,
-                  content,
-                  createdDateTime,
-                } = result;
-
-                return (
-                  <ReviewCard
-                    key={index}
-                    username={username}
-                    cakeNumber={cakeNumber}
-                    score={score}
-                    sheetType={sheetType}
-                    requestOption={requestOption}
-                    reviewImages={reviewImages}
-                    tags={tags}
-                    content={content}
-                    createdDateTime={createdDateTime}
-                  />
-                );
-              })}
-            </>
-          ) : (
-            <StyledEmptyContainer>
-              <Typography category={'Bd3'} color={'grey_300'}>
-                작성한 리뷰가 없어요!
+          {loading ? (
+            <div>
+              <div className="loading-icon">
+                <ClipLoader
+                  color={palette.white}
+                  loading={!loading}
+                  size={30}
+                />
+              </div>
+              <Typography category="Bd5" color="white">
+                내가 등록한 리뷰 불러오는 중...
               </Typography>
-              <StyledEmptyImg src={'/assets/images/empty-like-list.svg'} />
-              <StyledFindBtnContainer>
-                <Link href="/reviews/search">
-                  <Button size={'medium'} category={'primary'} disabled={false}>
-                    리뷰 작성하러 가기
-                  </Button>
-                </Link>
-              </StyledFindBtnContainer>
-            </StyledEmptyContainer>
+            </div>
+          ) : (
+            <>
+              {myReviewsList && myReviewsList.length > 0 ? (
+                <>
+                  <CountText>
+                    <Typography category={'Bd1'} color={'black'}>
+                      전체{' '}
+                    </Typography>
+                    <Typography category={'Bd1'} color={'cakeLavender_600'}>
+                      {myReviewsList.length}
+                    </Typography>
+                  </CountText>
+
+                  {myReviewsList.map((result: any, index: React.Key) => {
+                    const {
+                      username,
+                      cakeNumber,
+                      score,
+                      sheetType,
+                      requestOption,
+                      reviewImages,
+                      tags,
+                      content,
+                      createdDateTime,
+                    } = result;
+
+                    return (
+                      <ReviewCard
+                        key={index}
+                        username={username}
+                        cakeNumber={cakeNumber}
+                        score={score}
+                        sheetType={sheetType}
+                        requestOption={requestOption}
+                        reviewImages={reviewImages}
+                        tags={tags}
+                        content={content}
+                        createdDateTime={createdDateTime}
+                      />
+                    );
+                  })}
+                </>
+              ) : (
+                <StyledEmptyContainer>
+                  <Typography category={'Bd3'} color={'grey_300'}>
+                    작성한 리뷰가 없어요!
+                  </Typography>
+                  <StyledEmptyImg src={'/assets/images/empty-like-list.svg'} />
+                  <StyledFindBtnContainer>
+                    <Link href="/reviews/search">
+                      <Button
+                        size={'medium'}
+                        category={'primary'}
+                        disabled={false}
+                      >
+                        리뷰 작성하러 가기
+                      </Button>
+                    </Link>
+                  </StyledFindBtnContainer>
+                </StyledEmptyContainer>
+              )}
+            </>
           )}
         </Container>
 
         <Navigation type={'default'} my />
       </Section>
-      {loading ? (
-        <Absolute>
-          <LoadingContainer
-            text="마이페이지 데이터 불러오는 중..."
-            loaded={!loading}
-          />
-        </Absolute>
-      ) : null}
     </Main>
   );
 };
